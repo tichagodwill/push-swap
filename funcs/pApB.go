88d0e36f6dff36583e6873c0b5086835f738dfce
emahfood <eman.mjm@gmail.com>
2023-09-19 20:20:47 +0300

    PA&PB fix

--- funcs/pApB.go
+++ funcs/pApB.go
@@ -2,16 +2,20 @@ package funcs
 
 import "pushswap/stack"
 
-func PB(sA *stack.S, sB *stack.S) {
-	a, canPop := (*stack.S)(sA).Pop()
+func PB(sA *stack.S, sB *stack.S) bool {
+	a, canPop := sA.Pop()
 	if canPop {
 		*sB = append(*sB, a)
+		return true
 	}
+	return false
 }
 
-func PA(sA *stack.S, sB *stack.S) {
-	b, canPop := (*stack.S)(sB).Pop()
+func PA(sA *stack.S, sB *stack.S) bool {
+	b, canPop := sB.Pop()
 	if canPop {
 		*sA = append(*sA, b)
+		return true
 	}
+	return false
 }
