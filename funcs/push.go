082faa2a1bd5b33d12dbfab582098a16186946ef
emahfood <eman.mjm@gmail.com>
2023-09-19 07:24:23 +0300

    reading from the arguments

--- funcs/push.go
+++ funcs/push.go
@@ -1,25 +1,29 @@
 package funcs
 
-type Stack []string
+type (
+	StackA []int64
+	StackB []int64
+)
 
 // IsEmpty: check if stack is empty
-func (s *Stack) IsEmpty() bool {
+func (s *StackA) AIsEmpty() bool {
 	return len(*s) == 0
 }
 
 // Push a new value onto the stack
-func (s *Stack) Push(str string) {
-	*s = append(*s, str) // Simply append the new value to the end of the stack
+func (s *StackA) Push(n int64) {
+	*s = append(*s, n) // Simply append the new value to the end of the stack
+	// the end of the array will be the top of the stack
 }
 
 // Remove and return top element of stack. Return false if stack is empty.
-func (s *Stack) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
+func (s *StackA) Pop() (int64, bool) {
+	if s.AIsEmpty() {
+		return -1, false
 	} else {
-		index := len(*s) - 1 // Get the index of the top most element.
+		index := len(*s) - 1   // Get the index of the top most element.
 		element := (*s)[index] // Index into the slice and obtain the element.
-		*s = (*s)[:index] // Remove it from the stack by slicing it off.
+		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
 		return element, true
 	}
 }
--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"pushswap/funcs"
 )
 
 func main() {
-	var stack funcs.Stack // create a stack variable of type Stack
-	stack.Push("this")
-	stack.Push("is")
-	stack.Push("sparta!!")
-
-	for len(stack) > 0 {
-		x, y := stack.Pop()
+	if len(os.Args) != 2 {
+		fmt.Println("usage:")
+		return
+	}
+	var stacka funcs.StackA // create a stack variable of type Stack
+	numStr := strings.Fields(os.Args[1])
+	// Push from the last element to the first so the first will be on the top of the stack
+	for i := len(numStr) - 1; i >= 0; i-- {
+		n, err := strconv.ParseInt(numStr[i], 10, 64)
+		if err != nil {
+			fmt.Println("Error Parsing integer ",err)
+			return
+		}
+		stacka.Push(n)
+	}
+	for len(stacka) > 0 {
+		x, y := stacka.Pop()
 		if y {
 			fmt.Println(x)
 		}
